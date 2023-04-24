cf544195bad9bb1f1ae0bef44684493bf7e9c5b4
“advidalmeza” <avidalmeza@bren.ucsb.edu>
2023-04-23 17:06:05 -0700

    Add files

--- src/almond-yield.R
+++ src/almond-yield.R
@@ -7,11 +7,13 @@
 #' @export
 #'
 #' @examples
-#' 
+#' almond_yield(temp_feb = 10.7, precip_jan = 0.09)
 
 almond_yield <- function(temp_feb, precip_jan) {
   
   yield_anom <- (-0.015*temp_feb) + (-0.0046*(temp_feb^2)) + (-0.07*precip_jan) + (0.0043*(precip_jan^2)) + 0.28
   
-  return(paste("Almond yield anomaly:", yield_anom, "ton/acre"))
+  # return(paste("Almond yield anomaly:", yield_anom, "ton/acre"))
+  return(yield_anom)
+  
 }
